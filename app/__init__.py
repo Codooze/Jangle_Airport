fbb00f2d0117ddd816256374a37562f43182cb0f
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-10-27 22:41:42 -0500

    secret key

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 # brights our application together
 from flask import Flask
 app = Flask(__name__)
+app.secret_key = "something only you know"
 
 from app import All_Views  # - dicen que se debe hacer así con flask, para evitar no sé  que circular import
  #ctrl + shift + p / save withoud formating
