a595d26371e35bae7188f318bc97f50388e25864
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-10-28 07:44:41 -0500

    for run

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from app import app
 import os
+from socket import gethostname
 
 # - podemos crear más files "view" por ejemplo "adm_views", "usuario_views" "userAutorización_view"etc
 secret = os.urandom(16)
 app.secret_key = secret
 app.config['SESSION_TYPE'] = 'filesystem'
 if __name__ == "__main__":
-    app.run(debug=True)
+    if 'liveconsole' not in gethostname():
+        app.run()
+    else: app.run(debug=True)
