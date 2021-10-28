c72d00edb6a64114b150c6daec9e2c34ec128724
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-10-27 21:50:31 -0500

    db changes

--- app/db.py
+++ app/db.py
@@ -2,7 +2,7 @@ import pymysql
 
 
 def obtener_conexion():
-    return pymysql.connect(host='Jeison',  # probar con su usuario de sistema o localhost
-                                user='root',  # probar con root
+    return pymysql.connect(host='Jeison.mysql.pythonanywhere-services.com',  # probar con su usuario de sistema o localhost
+                                user='Jeison',  # probar con root
                                 password='admin*****',
                                 db='Jeison$default')
