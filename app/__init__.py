cabeb144440ec97e758fa665963a8d1dc658ea33
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-10-27 20:51:13 -0500

    not working

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 # brights our application together
 from flask import Flask
 app = Flask(__name__)
+
 from app import All_Views  # - dicen que se debe hacer así con flask, para evitar no sé  que circular import
  #ctrl + shift + p / save withoud formating
 from app import adm_views
