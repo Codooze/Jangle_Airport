80cd0fa9a116dd57783d1be1478342a1d125a639
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-10-27 22:57:58 -0500

    secret key issue

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 # brights our application together
+import os
 from flask import Flask
+
 app = Flask(__name__)
-app.secret_key = "something only you know"
+secret = os.urandom(16)
+app.secret_key = secret
 
 from app import All_Views  # - dicen que se debe hacer así con flask, para evitar no sé  que circular import
  #ctrl + shift + p / save withoud formating
